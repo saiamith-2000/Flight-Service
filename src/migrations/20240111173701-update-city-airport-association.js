a229de5fd754214bb6ab8c17069d3daf79d23c6b
saiamith2000 <saiamith2000@gmail.com>
2024-01-11 23:37:17 +0530

    Added City Id as foreign key at Airports table ``` npx sequelize migration:generate --name update-city-airport-association
    ```

---
+++ src/migrations/20240111173701-update-city-airport-association.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.addConstraint('Airports',{
+       type:'FOREIGN KEY',
+       fields:['cityId'],
+       name:'city_fk_constraint',
+       references:{
+        table:'cities',
+        field:'id',
+       },
+       onUpdate:'CASCADE',
+       onDELETE:'CASCADE'
+     });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('Airports','city_fk_constraint');
+  }
+};
+
+
+/*
+SELECT 
+  TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME
+FROM
+  INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE
+  REFERENCED_TABLE_SCHEMA = 'flights' 
+  AND REFERENCED_TABLE_NAME = 'airports';
+*/
