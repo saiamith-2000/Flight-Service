a3c9450ed18a24a9eebae0d15ffc62ac4dd1fa11
saiamith2000 <saiamith2000@gmail.com>
2024-01-13 06:25:45 +0530

    Added departure time arrival time constraint

--- src/middlewares/flight-middleware.js
+++ src/middlewares/flight-middleware.js
@@ -2,6 +2,7 @@ const {StatusCodes}=require('http-status-codes');
 
 const { ErrorResponse }=require('../utils/common');
 const { AppError } = require('../utils/errors/app-error');
+const compareTime = require('../utils/helpers/datetime-helpers');
 
 function validateCreateRequest(req, res,next){
     if(!req.body.flightNumber){
@@ -44,6 +45,11 @@ if(!req.body.totalSeats){
   ErrorResponse.error=new AppError(['total seats found in incorrect form'],StatusCodes.BAD_REQUEST);
   return res.status(StatusCodes.BAD_REQUEST).json({ErrorResponse});
 }
+if(!compareTime(req.body.arrivalTime,req.body.departureTime)){
+    ErrorResponse.message="Something went wrong while creating flight";
+    ErrorResponse.error=new AppError(['Arrival time should be greater than departure time'],StatusCodes.BAD_REQUEST);
+    return res.status(StatusCodes.BAD_REQUEST).json({ErrorResponse});
+  }
     next();
 }
 
